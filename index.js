b01f9a7c8841a16eadbbb12a93c3d356644f5839
Nour Belmabrouk <nourbelmabrouk11@gmail.com>
2022-04-26 00:59:38 +0100

    add test action

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+const helloWorld = () => {
+    return 'Hello World!'
+}
+
+module.exports={helloWorld}
